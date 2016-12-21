c6b1a33eac1ff9c2d8c80923ead216ab9a710cb5
MANHATTAN <MANHATTAN@RAMEEZ>
2016-12-21 13:02:08 +0500

    add user.sql file in userDB project

---
+++ dbo.users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[users] (
+    [u_Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Name]   NVARCHAR (50) NOT NULL,
+    [Email]  NVARCHAR (50) NOT NULL,
+    [Age]    INT           NOT NULL,
+    [Degree] NVARCHAR (50) NOT NULL,
+    [Gender] VARCHAR (50)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([u_Id] ASC)
+);
